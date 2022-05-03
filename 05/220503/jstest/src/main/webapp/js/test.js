92045067331e7c905afd0f89c74ea1a53398dbbd
eicosaa <fomalhaut612@naver.com>
2022-05-03 17:43:55 +0900

    220503

---
+++ 05/220503/jstest/src/main/webapp/js/test.js
@@ -0,0 +1,10 @@
+/* 주석 */
+let x; // var x;
+x = 10;
+console.log(x);
+
+x = 'test';
+console.log(x);
+
+
+alert('안녕하세요');
---
+++ 05/220503/test.js
@@ -0,0 +1,5 @@
+let x;
+x = 10;
+console.log(x);
+x = 'test';
+console.log(x);
